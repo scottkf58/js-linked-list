51963ae3348b47a79280b58251b20614c848019c
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-24 22:53:30 -1000

    add function working

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,58 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
 
+var myLinkedList = linkedListGenerator();
+
+function linkedListGenerator(){
+  var head = null;
+  var tail = null;
+
+  function getHead() {
+    return head;
+
+  }
+
+  function getTail() {
+    return tail;
+
+  }
+
+  function add(value) {
+    var newNode = {
+        value: value,
+        next: null
+      };
+
+      if (head === null) {
+        head = newNode;
+        tail = newNode;
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+      }
+
+      return newNode;
+
+  }
+
+  function get(number) {
+
+  }
+
+  function remove(number) {
+
+  }
+
+  function insert(value, number) {
+
+  }
+  return {
+    getHead,
+    getTail,
+    add,
+    remove,
+    get,
+    insert,
+  }
 }
